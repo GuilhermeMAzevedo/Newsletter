b83cc98305966bc1ef245e53e462c040be044c17
GuilhermeMAzevedo <guilhermemazevedo2007@gmail.com>
2026-03-17 18:28:42 -0300

    Salvar tema escolhido anteriormente, validação do email e do nome, ajustes finais

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-//Selecionar elementos
 const root = document.documentElement;
 const btn = document.getElementById("btnTema");
 const ano = document.getElementById("ano");
+const temaSalvo = localStorage.getItem("tema");
+const prefereEscuro = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-//Preenchendo o ano atual
+if (temaSalvo === "escuro"){
+    root.setAttribute("data-tema", "escuro");
+}
+
 ano.textContent = new Date().getFullYear();
 
-//Tema light/dark
-
 btn.addEventListener("click", () => {
-    const escuro = root.getAttribute("data-tema") === 'escuro'
+    const escuro = root.getAttribute("data-tema") === 'escuro';
 
     if (escuro){
-        root.removeAttribute("data-tema")
+        root.removeAttribute("data-tema");
+        localStorage.setItem("tema", "claro");
     } else {
-        root.setAttribute("data-tema", "escuro")
+        root.setAttribute("data-tema", "escuro");
+        localStorage.setItem("tema", "escuro");
     }
+});
+
+document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+
+    alert("Inscrição realizada!");
 })
